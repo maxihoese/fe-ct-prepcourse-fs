63c927745db0b18f4831f40805783da4f63be0f2
maxihoese <maxihoese@gmail.com>
2023-04-24 02:53:41 -0300

    Homework M09 resuelto

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,12 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var parametros = Object.keys(objeto);
+   var arregloPadre = [];
+   for (i = 0; i < parametros.length; i++) {
+      arregloPadre.push([parametros[i], objeto[parametros[i]]]);
+   }
+   return arregloPadre;
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +20,19 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   var stringOrdenado = string.split("").sort();
+   var NewObject = {};
+   for (i = 0; i < stringOrdenado.length; i++) {
+      let propiedad = stringOrdenado[i]
+      NewObject[propiedad] = 1;
+      }
+   for (i = 1; i < stringOrdenado.length; i++) {
+      if (stringOrdenado[i] == stringOrdenado[i-1]) {
+         let propiedad = stringOrdenado[i];
+         NewObject[propiedad] = NewObject[propiedad] + 1;
+      }
+   }
+   return NewObject;
 }
 
 function capToFront(string) {
@@ -22,6 +41,14 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   var arregloDeString = string.split("")
+   var mayus = [];
+   var minus = [];
+   for (i = 0; i < arregloDeString.length; i++) {
+      if (arregloDeString[i] == arregloDeString[i].toUpperCase()) mayus.push(arregloDeString[i]);
+      else minus.push(arregloDeString[i]);
+   }
+   return mayus.join("") + minus.join("");
 }
 
 function asAmirror(frase) {
@@ -29,18 +56,28 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   let invertido = frase.split('').reverse().join('');
+   return invertido.split(' ').reverse().join(' ');
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   var numInvertido = numero.toString().split('').reverse().join('');
+   if (numero == numInvertido) return "Es capicua";
+   else return "No es capicua";
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   var separado = string.split('');
+   for (i = string.length - 1; i >= 0; i--) {
+      if (separado[i] == "a" || separado[i] == "b" || separado[i] == "c") separado.splice(i,1);
+   }
+   return separado.join('');
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +86,15 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   // var arregloOrdenado = [arrayOfStrings[0]];
+   // for (i = 1; i < arrayOfStrings.length; i++) {
+   //    if (arrayOfStrings[i].length >= arregloOrdenado.at(-1).length) {
+   //       arregloOrdenado.push(arrayOfStrings[i]);
+   //    }
+   //    else arregloOrdenado.un(arrayOfStrings[i])
+   // }
+   var nuevoArreglo = arrayOfStrings.sort((a,b) => a.length - b.length);
+   return nuevoArreglo;
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +104,17 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   var array3 = [];
+   if (array1.length >= array2.length) {
+      for (i = 0; i < array1.length; i++) {
+         let elemento = array1[i];
+         if (array2.includes(elemento)) array3.push(elemento);
+      }
+   } else for (i = 0; i < array2.length; i++) {
+         let elemento = array2[i];
+         if (array1.includes(elemento)) array3.push(elemento);
+   }
+   return array3;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
